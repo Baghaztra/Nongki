9e6bd5c141c98e4eb34ea9973a22cd52e2ee61a5
Windy Anggreni <126459564+windyanggreni@users.noreply.github.com>
2024-06-22 23:19:59 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,3 +15,7 @@ Route::get('/home', function () {
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
