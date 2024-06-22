2b3a21d6a0593d8526c186010ee6a7b7a4b75088
Baghaztra <127162951+Baghaztra@users.noreply.github.com>
2024-06-22 22:04:27 +0700

    home

--- public/js/home.js
+++ public/js/home.js
@@ -1,1 +1,101 @@
-const container = document.getElementById()
+// const container = document.getElementById()
+
+// console.log('bjir');
+
+// Ganti image
+document.addEventListener('DOMContentLoaded', function() {
+    var images = document.querySelectorAll('.image');
+    images.forEach(function(image) {
+      var imagePath = image.getAttribute('data-image');
+      if (imagePath) {
+        image.style.backgroundImage = 'url(' + imagePath + ')';
+        image.style.backgroundSize = 'cover'; // Sesuaikan dengan kebutuhan
+        image.style.backgroundPosition = 'center'; // Sesuaikan dengan kebutuhan
+      }
+    });
+  });
+
+// Modal
+document.addEventListener('DOMContentLoaded', (event) => {
+    const detailModal = document.getElementById('details-modal');
+    detailModal.addEventListener('show.bs.modal', (event) => {
+        const button = event.relatedTarget;
+
+        const cornerName = detailModal.querySelector('#corner-name');
+        const detail = detailModal.querySelector('#detail');
+        const categories = detailModal.querySelector('#categories');
+        const facilities = detailModal.querySelector('#facilities');
+        const images = detailModal.querySelector('#images');
+        const images_indicator = detailModal.querySelector('#images-indicator');
+        const location = detailModal.querySelector('#location');
+
+        cornerName.innerHTML = button.getAttribute('data-name');
+        detail.innerHTML = button.getAttribute('data-detail');
+        location.setAttribute('href', button.getAttribute('data-location'));
+        
+
+        // Bikin gambar
+        const objectGambar = JSON.parse(button.getAttribute('data-images'));
+        
+        images.innerHTML = '';
+        images_indicator.innerHTML = '';
+
+        objectGambar.forEach((item, index) => {
+            // console.log(item);
+            
+            const carouselItem = document.createElement('div');
+            carouselItem.classList.add('carousel-item');
+            if (index === 0) {
+                carouselItem.classList.add('active');
+            }
+
+            const img = document.createElement('img');
+            img.classList.add('d-block', 'w-100');
+            img.setAttribute('src', item.path);
+            img.setAttribute('alt', item.path);
+
+            carouselItem.appendChild(img);
+            images.appendChild(carouselItem);
+
+            const indicator = document.createElement('button');
+            indicator.type = 'button';
+            indicator.setAttribute('data-bs-target', '#slider');
+            indicator.setAttribute('data-bs-slide-to', index);
+            if (index === 0) {
+                indicator.classList.add('active');
+                indicator.setAttribute('aria-current', 'true');
+            }
+            indicator.setAttribute('aria-label', 'Slide ' + (index + 1));
+
+            images_indicator.appendChild(indicator);
+        });
+
+        // bikin kategori
+        const objectKategori = JSON.parse(button.getAttribute('data-categories'));
+        categories.innerHTML='';
+        objectKategori.forEach(item => {
+          // console.log(item);
+          const list = document.createElement('li');
+          list.innerHTML=item.name;
+          categories.appendChild(list);
+        });
+
+        // bikin kategori
+        const objectFasilitas = JSON.parse(button.getAttribute('data-facilities'));
+        facilities.innerHTML='';
+        objectFasilitas.forEach(item => {
+          // console.log(item);
+          const list = document.createElement('li');
+          list.innerHTML=item.name;
+          facilities.appendChild(list);
+        });
+
+        
+        // harga.innerHTML= parseFloat(button.getAttribute('data-f')).toLocaleString('id-ID', {
+        //     style: 'currency',
+        //     currency: 'IDR',
+        //     minimumFractionDigits: 2
+        // });
+
+    });
+});
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -25,7 +25,7 @@
             </div>
         </nav>
     </header>
-    <main>
+    <main class="shadow">
         <div class="top">
             <div class="search">
                 <input type="search" id="search" placeholder="Temukan tempat nongki anda...">
@@ -36,65 +36,56 @@
             </div>
         </div>
         <div class="corners">
-            @for($i = 0; $i <= 15; $i++)
-                <div class="corner">
-                <h2 class="name">Pondok Aciak Jaya</h2>
+            @foreach($corners as $item)
+              <div class="corner shadow mb-2 mt-1" data-bs-toggle="modal" data-bs-target="#details-modal"
+                  data-name="{{ $item->name }}" 
+                  data-location="{{ $item->location }}" 
+                  data-detail="{{ $item->detail }}"
+                  data-images="{{ $item->images }}"
+                  data-categories="{{ $item->categories }}"
+                  data-facilities="{{ $item->facilities }}">
+                <h2 class="name">{{ $item->name }}</h2>
                 <p class="categories">
-                    <span class="category">Cafe</span>, 
-                    <span class="category"> Workspace</span>,
-                    <span class="category"> Khodams</span>,
-                    <span class="category"> Restaurant</span>
+                  <i class="category">{{ $item->categories->pluck('name')->implode(', ') }}</i>
                 </p>
-                <div class="image">
+                <div class="image" data-image="{{ $item->images[0]->path }}">
                     <div class="left">
                         <div class="facilities">
-                            <span class="facility">Wifi</span>, 
-                            <span class="facility"> Electricity</span>,
-                            <span class="facility"> AC</span>,
-                            <span class="facility"> Toilet</span>
+                          <span class="facility">{{ $item->facilities->pluck('name')->implode(' | ') }}</span>
                         </div>
-                        <div class="location">
+                        <div class="location" target="_balnk">
                             <i class="fa-solid fa-earth"></i>
-                            Pauh, Padang
+                            @php
+                              $cities = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Jeruk Manis'];
+                              echo $cities[array_rand($cities)];
+                            @endphp
                         </div>
                     </div>
-                    <div class="right">
+                    {{-- <a href="{{ $item->location }}" class="right">
                         <button type="button">View</button>
-                    </div>
+                    </a> --}}
                 </div>
                 <div class="description">
                     <div class="detail flex flex-row">
                         <div class="icons">
                             <i class="fa-solid fa-circle-info"></i>
                         </div>
-                        <div class="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolorum neque repellat nam ab quas. <span class="read">Selengkapnya...</span></div>
+                        <div class="paragraph">{{ substr($item->detail, 0, 100) }}{!! strlen($item->detail) > 100 ? '<span>...</span>' : '' !!} </div>
                     </div>
                     <p class="status"><span class="icons"><i class="fa-solid fa-clock"></i></span> Tutup pukul 23.00</p>
-                    <ul class="others">
+                    {{-- <ul class="others">
                         <li class="other">Menu</li>
                         <li class="other">Service</li>
                         <li class="other">Contact</li>
-                    </ul>
+                    </ul> --}}
                 </div>
-            @endfor
-            </div>
+              </div>
+            @endforeach
         </div>
     </main>
 
-    @foreach ($corners as $item)
-      <button class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#details-modal"
-        data-name="{{ $item->name }}" 
-        data-location="{{ $item->location }}" 
-        data-detail="{{ $item->detail }}"
-        data-images="{{ $item->images }}"
-        data-categories="{{ $item->categories }}"
-        data-facilities="{{ $item->facilities }}">
-        Detail
-      </button>
-    @endforeach
-
     <!-- Modal -->
-    <div class="modal fade card" id="details-modal" tabindex="-1" aria-hidden="true">
+    <div class="modal fade" id="details-modal" tabindex="-1" aria-hidden="true">
       <div class="modal-dialog modal-dialog-scrollable modal-lg" >
           <div class="modal-content">
               <div class="modal-header">
@@ -131,92 +122,7 @@
       </div>
   </div>
 
-  <script>
-      document.addEventListener('DOMContentLoaded', (event) => {
-          const detailModal = document.getElementById('details-modal');
-          detailModal.addEventListener('show.bs.modal', (event) => {
-              const button = event.relatedTarget;
-
-              const cornerName = detailModal.querySelector('#corner-name');
-              const detail = detailModal.querySelector('#detail');
-              const categories = detailModal.querySelector('#categories');
-              const facilities = detailModal.querySelector('#facilities');
-              const images = detailModal.querySelector('#images');
-              const images_indicator = detailModal.querySelector('#images-indicator');
-              const location = detailModal.querySelector('#location');
-
-              cornerName.innerHTML = button.getAttribute('data-name');
-              detail.innerHTML = button.getAttribute('data-detail');
-              location.setAttribute('href', button.getAttribute('data-location'));
-              
-
-              // Bikin gambar
-              const objectGambar = JSON.parse(button.getAttribute('data-images'));
-              
-              images.innerHTML = '';
-              images_indicator.innerHTML = '';
-
-              objectGambar.forEach((item, index) => {
-                  // console.log(item);
-                  
-                  const carouselItem = document.createElement('div');
-                  carouselItem.classList.add('carousel-item');
-                  if (index === 0) {
-                      carouselItem.classList.add('active');
-                  }
-
-                  const img = document.createElement('img');
-                  img.classList.add('d-block', 'w-100');
-                  img.setAttribute('src', item.path);
-                  img.setAttribute('alt', item.path);
-
-                  carouselItem.appendChild(img);
-                  images.appendChild(carouselItem);
-
-                  const indicator = document.createElement('button');
-                  indicator.type = 'button';
-                  indicator.setAttribute('data-bs-target', '#slider');
-                  indicator.setAttribute('data-bs-slide-to', index);
-                  if (index === 0) {
-                      indicator.classList.add('active');
-                      indicator.setAttribute('aria-current', 'true');
-                  }
-                  indicator.setAttribute('aria-label', 'Slide ' + (index + 1));
-
-                  images_indicator.appendChild(indicator);
-              });
-
-              // bikin kategori
-              const objectKategori = JSON.parse(button.getAttribute('data-categories'));
-              categories.innerHTML='';
-              objectKategori.forEach(item => {
-                // console.log(item);
-                const list = document.createElement('li');
-                list.innerHTML=item.name;
-                categories.appendChild(list);
-              });
-
-              // bikin kategori
-              const objectFasilitas = JSON.parse(button.getAttribute('data-facilities'));
-              facilities.innerHTML='';
-              objectFasilitas.forEach(item => {
-                // console.log(item);
-                const list = document.createElement('li');
-                list.innerHTML=item.name;
-                facilities.appendChild(list);
-              });
-
-              
-              // harga.innerHTML= parseFloat(button.getAttribute('data-f')).toLocaleString('id-ID', {
-              //     style: 'currency',
-              //     currency: 'IDR',
-              //     minimumFractionDigits: 2
-              // });
-
-          });
-      });
-  </script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script src="js/home.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
