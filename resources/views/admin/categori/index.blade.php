ae91562e2c6fab0f95d6d28b395ca19d660aeff7
Pito Desri Pauzi <pitokfauzi@gmail.com>
2024-06-22 12:26:52 +0700

    nambain custom.css

--- resources/views/admin/categori/index.blade.php
+++ resources/views/admin/categori/index.blade.php
@@ -2,5 +2,11 @@
 
 @section('title', 'Kategori')
 @section('content')
-    test
+    <div class="card shadow mt-3">
+        <div class="card-header d-flex justify-content-between">Kategori <button class="btn btn-sm btn-primary">add</button>
+        </div>
+        <div class="card-body">
+            data
+        </div>
+    </div>
 @endsection
