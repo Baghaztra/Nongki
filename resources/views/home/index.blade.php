dc028c55dea361cf50f431cf6426772a09dddda1
Topan Sidiq <topansidiq28@gmail.com>
2024-06-22 17:17:36 +0700

    header home
    Biar seperti ada kerjaan

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,21 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Nongki | Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/home.css">
   </head>
   <body>
     <header>
-        <div class="logo container">
-            <img src="/assets/img/logo.png" alt="">
+        <div class="container-flex brand">
+            <div class="logo">
+                <img src="/assets/img/logo.png" alt="">
+            </div>
+            <p class="title">Nongki</p>
         </div>
-        <h2 class="title text-lg">Nongki</h2>
+        <div class="dark-mode">
+
+        </div>
+        <nav class="container-flex navbar">
+            <div class="nav-item flex">
+                <button class="button">Sarankan tempat</button>
+            </div>
+        </nav>
     </header>
     <main>
-        <div class="container bg-slate-400">
-            @foreach ($corners as $corner)
-                
-            @endforeach
-        </div>
+        
     </main>
 
 
