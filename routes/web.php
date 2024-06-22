52cb1b74b5abde20fa91da0f339c3ed570e3f948
Baghaztra <127162951+Baghaztra@users.noreply.github.com>
2024-06-23 00:49:59 +0700

    apalah

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::resource('admin/dashboard', AdminController::class);
-Route::get('get-data-recomendation', [AdminController::class, 'getAllData']);
-Route::resource('admin/corner', CornerController::class);
-Route::resource('admin/categories', CategoryController::class);
-Route::resource('admin/facilities', FacilityController::class);
-Route::resource('admin/users', UserController::class);
-Route::get('get-data-corner', [CornerController::class, 'getAllData']);
-Route::get('get-data-categories', [CategoryController::class, 'getAllData']);
-Route::get('get-data-facilities', [FacilityController::class, 'getAllData']);
-Route::get('get-data-users', [UserController::class, 'getAllData']);
-
-
-
 Route::get('/home', [FECornerController::class, 'index']);
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'authenticate']);
-Route::post('/logout', [LoginController::class, 'logout']);
+Route::middleware(['auth'])->group(function () {
+    Route::resource('admin/dashboard', AdminController::class);
+    Route::get('get-data-recomendation', [AdminController::class, 'getAllData']);
+    Route::resource('admin/corner', CornerController::class);
+    Route::resource('admin/categories', CategoryController::class);
+    Route::resource('admin/facilities', FacilityController::class);
+    Route::resource('admin/users', UserController::class);
+    Route::get('get-data-corner', [CornerController::class, 'getAllData']);
+    Route::get('get-data-categories', [CategoryController::class, 'getAllData']);
+    Route::get('get-data-facilities', [FacilityController::class, 'getAllData']);
+    Route::get('get-data-users', [UserController::class, 'getAllData']);
+
+    
+    Route::get('/logout', [LoginController::class, 'logout']);
+});
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'authenticate']);
+});
