03aa5e55281b804a30c9c4e9c7d1692ab566977c
Baghaztra <127162951+Baghaztra@users.noreply.github.com>
2024-06-22 20:38:20 +0700

    modal

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,27 +22,45 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         
+        $pojok = 10;
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
 
-        Corner::factory(10)->create();
+        Corner::factory($pojok)->create();
         Facility::factory(10)->create();
         Category::factory(10)->create();
+        
+        // minimal 1 fasilitas untuk tiap corner 
+        for ($i=1; $i <= $pojok; $i++) { 
+            CornerFacilities::factory()->create([
+                'facility_id' => Facility::all()->random()->id,
+                'corner_id' => $i,
+            ]);
+        }
         CornerFacilities::factory(10)->create();
+        
+        // minimal 1 category untuk tiap corner 
+        for ($i=1; $i <= $pojok; $i++) { 
+            CornerCategories::factory()->create([
+                'category_id' => Category::all()->random()->id,
+                'corner_id' => $i,
+            ]);
+        }
         CornerCategories::factory(10)->create();
 
         // minimal 1 gambar untuk tiap corner 
-        for ($i=1; $i <= 10; $i++) { 
+        for ($i=1; $i <= $pojok; $i++) { 
             Image::factory()->create([
-                'path' => ['dummy1.jpg', 'dummy2.jpg', 'dummy3.jpg'][rand(0, 2)],
+                'path' => '/images/'.['dummy1.jpg', 'dummy2.jpg', 'dummy3.jpg'][rand(0, 2)],
                 'corner_id' => $i,
             ]);
         }
         Image::factory(10)->create();
         
-        // Feedback
+        // Feedback dari 
         Recomendation::factory(20)->create();
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -29,7 +29,141 @@
         
     </main>
 
+    @foreach ($corners as $item)
+      <button class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#details-modal"
+        data-name="{{ $item->name }}" 
+        data-location="{{ $item->location }}" 
+        data-detail="{{ $item->detail }}"
+        data-images="{{ $item->images }}"
+        data-categories="{{ $item->categories }}"
+        data-facilities="{{ $item->facilities }}">
+        Detail
+      </button>
+    @endforeach
 
+    <!-- Modal -->
+    <div class="modal fade card" id="details-modal" tabindex="-1" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-scrollable modal-lg" >
+          <div class="modal-content">
+              <div class="modal-header">
+                <strong class="modal-title fs-2"  id="corner-name"></strong>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <div id="slider" class="carousel slide mb-3">
+                  <div class="carousel-indicators" id="images-indicator"></div>
+                  <div class="carousel-inner" id="images"></div>
+                  <button class="carousel-control-prev" type="button" data-bs-target="#slider" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                  </button>
+                  <button class="carousel-control-next" type="button" data-bs-target="#slider" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                  </button>
+                </div>
+                <div id="detail" class="mb-3"></div>
+                <div class="row">
+                  <div class="col-md-6 mb-3">
+                    <strong>Categories</strong>
+                    <ul id="categories" class="list-unstyled text-small"></ul>
+                  </div>
+                  <div class="col-md-6 mb-3">
+                    <strong>Facilities</strong>
+                    <ul id="facilities" class="list-unstyled text-small"></ul>
+                  </div>
+                </div>
+                <a id="location" href="" target="_blank">Lihat lokasi</a>
+              </div>
+          </div>
+      </div>
+  </div>
+
+  <script>
+      document.addEventListener('DOMContentLoaded', (event) => {
+          const detailModal = document.getElementById('details-modal');
+          detailModal.addEventListener('show.bs.modal', (event) => {
+              const button = event.relatedTarget;
+
+              const cornerName = detailModal.querySelector('#corner-name');
+              const detail = detailModal.querySelector('#detail');
+              const categories = detailModal.querySelector('#categories');
+              const facilities = detailModal.querySelector('#facilities');
+              const images = detailModal.querySelector('#images');
+              const images_indicator = detailModal.querySelector('#images-indicator');
+              const location = detailModal.querySelector('#location');
+
+              cornerName.innerHTML = button.getAttribute('data-name');
+              detail.innerHTML = button.getAttribute('data-detail');
+              location.setAttribute('href', button.getAttribute('data-location'));
+              
+
+              // Bikin gambar
+              const objectGambar = JSON.parse(button.getAttribute('data-images'));
+              
+              images.innerHTML = '';
+              images_indicator.innerHTML = '';
+
+              objectGambar.forEach((item, index) => {
+                  // console.log(item);
+                  
+                  const carouselItem = document.createElement('div');
+                  carouselItem.classList.add('carousel-item');
+                  if (index === 0) {
+                      carouselItem.classList.add('active');
+                  }
+
+                  const img = document.createElement('img');
+                  img.classList.add('d-block', 'w-100');
+                  img.setAttribute('src', item.path);
+                  img.setAttribute('alt', item.path);
+
+                  carouselItem.appendChild(img);
+                  images.appendChild(carouselItem);
+
+                  const indicator = document.createElement('button');
+                  indicator.type = 'button';
+                  indicator.setAttribute('data-bs-target', '#slider');
+                  indicator.setAttribute('data-bs-slide-to', index);
+                  if (index === 0) {
+                      indicator.classList.add('active');
+                      indicator.setAttribute('aria-current', 'true');
+                  }
+                  indicator.setAttribute('aria-label', 'Slide ' + (index + 1));
+
+                  images_indicator.appendChild(indicator);
+              });
+
+              // bikin kategori
+              const objectKategori = JSON.parse(button.getAttribute('data-categories'));
+              categories.innerHTML='';
+              objectKategori.forEach(item => {
+                // console.log(item);
+                const list = document.createElement('li');
+                list.innerHTML=item.name;
+                categories.appendChild(list);
+              });
+
+              // bikin kategori
+              const objectFasilitas = JSON.parse(button.getAttribute('data-facilities'));
+              facilities.innerHTML='';
+              objectFasilitas.forEach(item => {
+                // console.log(item);
+                const list = document.createElement('li');
+                list.innerHTML=item.name;
+                facilities.appendChild(list);
+              });
+
+              
+              // harga.innerHTML= parseFloat(button.getAttribute('data-f')).toLocaleString('id-ID', {
+              //     style: 'currency',
+              //     currency: 'IDR',
+              //     minimumFractionDigits: 2
+              // });
+
+          });
+      });
+  </script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
